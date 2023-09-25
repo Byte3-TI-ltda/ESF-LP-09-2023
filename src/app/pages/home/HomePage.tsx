ebdc4a1147ea295f562e237f908e0b93be6355a4
VitorHugoAntunes <vitor4ntunes@gmail.com>
2023-09-24 21:08:04 -0300

    Page Counter

--- src/app/pages/home/HomePage.tsx
+++ src/app/pages/home/HomePage.tsx
@@ -5,25 +5,52 @@ import { Carousel } from "react-bootstrap";
 import { Section } from "../../shared/components/organisms/Section/Section";
 import { BasicTemplate } from "../../shared/components/templates/BasicTemplate/BasicTemplate";
 import { WelcomeFrame } from "../../shared/components/molecules/WelcomeFrame/WelcomeFrame";
-
-const ItemWelcomeFrame = () => {
-  return <WelcomeFrame />;
-};
-
-const ItemFrameSignUp = () => {
-  return <ApplicationForm />;
-};
+import { useState } from "react";
+import { SocialButton } from "../../shared/components/atoms/SocialButton/SocialButton";
 
 const HomeCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: number) => {
+    setActiveIndex(selectedIndex);
+  };
+
+  const slides = [
+    {
+      id: 1,
+      component: <WelcomeFrame />,
+    },
+    {
+      id: 2,
+      component: <ApplicationForm />,
+    },
+  ];
+
   return (
-    <Carousel controls={false} interval={null}>
-      <Carousel.Item className="slide">
-        <Section children={ItemWelcomeFrame()} />
-      </Carousel.Item>
-      <Carousel.Item className="slide">
-        <Section children={ItemFrameSignUp()} />
-      </Carousel.Item>
-    </Carousel>
+    <div>
+      <Carousel activeIndex={activeIndex} onSelect={handleSelect} controls={false} interval={null}>
+
+        {slides.map(slide => (
+          <Carousel.Item key={slide.id} className="slide">
+            <Section children={slide.component} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+
+      <div className="slideCounter d-flex flex-1 justify-content-between w-75">
+        <div onClick={() => setActiveIndex(activeIndex - 1)} className={activeIndex !== 0 ? "" : "invisible"}>
+          <SocialButton value={"-"} />
+        </div>
+
+        <span>
+          {activeIndex + 1} de {slides.length}
+        </span>
+
+        <div onClick={() => setActiveIndex(activeIndex + 1)} className={activeIndex !== slides.length - 1 ? "" : "invisible"}>
+          <SocialButton value={"+"} />
+        </div>
+      </div>
+    </div>
   );
 };
 
